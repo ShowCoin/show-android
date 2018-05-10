4942b98c45a0a056048ac93e8f53cca9fbc1940f
clarkMIssIon <aguan@gmail.com>
2018-05-10 16:09:15 +0800

    add voice msg player

---
+++ app/src/main/java/one/show/live/showlive/audio/IVoicePlayerInfoListener.java
@@ -0,0 +1,12 @@
+package one.show.live.audio;
+
+/**
+ * Created by clarkM1ss1on on 2018/5/8
+ */
+public interface IVoicePlayerInfoListener {
+    public void onPlayerGainFocus();
+
+    public void onPlayerStop();
+
+    public void onPlayerError();
+}
