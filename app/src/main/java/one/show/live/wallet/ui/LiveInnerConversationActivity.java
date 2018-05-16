7dd96a24e65c8a5e466be7ec408786ff9c3281c8
clarkMIssIon <aguan@gmail.com>
2018-05-16 21:34:30 +0800

    add view for live inner im

---
+++ app/src/main/java/one/show/live/wallet/ui/LiveInnerConversationActivity.java
@@ -0,0 +1,18 @@
+package one.show.live.message.ui;
+
+import android.os.Bundle;
+
+import one.show.live.common.ui.BaseFragmentActivity;
+
+/**
+ * Created by clarkM1ss1on on 2018/5/16
+ */
+public class LiveInnerConversationActivity
+        extends BaseFragmentActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
