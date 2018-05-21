150c0d0c07a07f388fc810b1e491a8f542524bb2
clarkMIssIon <aguan@gmail.com>
2018-05-21 22:07:18 +0800

    add conversation related presenters

---
+++ app/src/main/java/one/show/live/message/presenter/ContractPresenter.java
@@ -0,0 +1,13 @@
+package one.show.live.message.presenter;
+
+import one.show.live.common.ui.BasePresenter;
+
+/**
+ * Created by clarkM1ss1on on 2018/5/4
+ */
+public class ContractPresenter extends BasePresenter {
+
+    public void requestToGetContract() {
+
+    }
+}
