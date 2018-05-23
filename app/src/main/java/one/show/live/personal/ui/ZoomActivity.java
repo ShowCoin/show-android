2ea6d1533efd367a6d23326f50b2ed66bbc5573a
liuzehua <liu_ze_hua@sina.com>
2018-05-23 15:21:56 +0800

    ac

---
+++ app/src/main/java/one/show/live/personal/ui/ZoomActivity.java
@@ -0,0 +1,190 @@
+package one.show.live.personal.ui;
+
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.view.ViewTreeObserver;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.LinearLayout;
+
+import com.facebook.drawee.view.SimpleDraweeView;
+
+import java.math.BigDecimal;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import one.show.live.R;
+import one.show.live.common.ui.BaseFragmentActivity;
+import one.show.live.common.util.DeviceUtils;
+import one.show.live.common.util.FrescoUtils;
+import one.show.live.log.Logger;
+
+/**
+ * Created by Nano on Show.
+ */
+
+public class ZoomActivity extends BaseFragmentActivity {
+    @BindView(R.id.zoom_img)
+    SimpleDraweeView zoomImg;
+
+    int DEFAULT_DURATION = 3000;
+
+    public final static AccelerateInterpolator ACC_INTERPOLATOR = new AccelerateInterpolator(
+            10.0f);
+    int deltaX;
+    int deltaY;
+    float scaleX;
+    float scaleY;
+
+
+    int originViewLeft;
+    int originViewTop;
+    int originViewWidth;
+    int originViewHeight;
+
+    String imageUrl;
+    @BindView(R.id.zoom_lay)
+    LinearLayout zoomLay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_zoom);
+        ButterKnife.bind(this);
+//        initStatusBar(ContextCompat.getColor(this, R.color.color_333333), zoomLay);
+        initTransparentWindow();
+
+        // 取出传递过来的originView信息
+        extractViewInfoFromBundle();
+        FrescoUtils.bind(zoomImg, imageUrl);
+        onUiReady();
+
+        zoomImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+    }
+
+    private void onUiReady() {
+        zoomImg.getViewTreeObserver().addOnPreDrawListener(new ViewTreeObserver.OnPreDrawListener() {
+            @Override
+            public boolean onPreDraw() {
+                // remove previous listener
+                zoomImg.getViewTreeObserver().removeOnPreDrawListener(this);
+                //准备场景
+                prepareScene();
+                //播放动画
+                runEnterAnimation();
+                return true;
+            }
+        });
+    }
+
+
+    private void extractViewInfoFromBundle() {
+        Bundle bundle = getIntent().getExtras();
+        originViewLeft = bundle.getInt("left");
+        originViewTop = bundle.getInt("top");
+        originViewWidth = bundle.getInt("width");
+        originViewHeight = bundle.getInt("height");
+        imageUrl = bundle.getString("img");
+
+
+    }
+
+    private void prepareScene() {
+        //缩放到起始view大小
+        scaleX = (float) originViewWidth / zoomImg.getWidth();
+        scaleY = (float) originViewHeight / zoomImg.getHeight();
+
+        if(setLp()==1){
+            scaleY = scaleX;
+
+        }else if(setLp()==2){
+            scaleX = scaleY;
+        }
+
+        zoomImg.setScaleX(scaleX);
+        zoomImg.setScaleY(scaleY);
+
+        int[] screenLocation = new int[2];
+        zoomImg.getLocationOnScreen(screenLocation);
+        //移动到起始view位置
+        deltaX = originViewLeft - screenLocation[0];
+        deltaY = originViewTop - screenLocation[1];
+        zoomImg.setTranslationX(deltaX);
+        zoomImg.setTranslationY(deltaY);
+
+
+
+    }
+
+    /**
+     * 设置和小图一样的比例
+     */
+    public int setLp() {
+        int ii=0;//1是用宽的比例2是用高的比例
+        double test1 = zoomImg.getWidth() / originViewWidth;
+        double test2 = zoomImg.getHeight() / originViewHeight;
+
+        BigDecimal s1 = new BigDecimal(test1);
+        BigDecimal s2 = new BigDecimal(test2);
+
+        if (s1.compareTo(s2) > 0) {//s1大
+            ii = 2;
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) zoomImg.getLayoutParams();
+            layoutParams.width = (int) (originViewWidth * test2);
+            layoutParams.height = (int) (originViewHeight * test2);
+            zoomImg.setLayoutParams(layoutParams);
+
+
+        } else if (s1.compareTo(s2) < 0) {
+            ii = 1;
+            LinearLayout.LayoutParams layoutParams = (LinearLayout.LayoutParams) zoomImg.getLayoutParams();
+            layoutParams.width = (int) (originViewWidth * test1);
+            layoutParams.height = (int) (originViewHeight * test1);
+            zoomImg.setLayoutParams(layoutParams);
+
+        }
+        return ii;
+    }
+
+    private void runEnterAnimation() {
+        zoomImg.setVisibility(View.VISIBLE);
+        //执行动画
+        zoomImg.animate()
+                .setDuration(DEFAULT_DURATION)
+//                .setInterpolator(ACC_INTERPOLATOR)
+                .scaleX(1f)
+                .scaleY(1f)
+                .translationX(0)
+                .translationY(0)
+                .start();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        runExitAnimation();
+    }
+
+    private void runExitAnimation() {
+        zoomImg.animate()
+                .setDuration(DEFAULT_DURATION)
+//                .setInterpolator(ACC_INTERPOLATOR)
+                .scaleX(scaleX)
+                .scaleY(scaleY)
+                .translationX(deltaX)
+                .translationY(deltaY)
+                .withEndAction(new Runnable() {
+                    @Override
+                    public void run() {
+                        finish();
+                        overridePendingTransition(0, 0);
+                    }
+                }).start();
+    }
+
+}
