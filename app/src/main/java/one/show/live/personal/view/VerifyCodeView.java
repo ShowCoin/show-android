8430e8b129549de5c4cafe51992d5b19ca6180ae
lihui <hui_tianyi@163.com>
2018-07-30 20:14:32 +0800

    add verifycode

---
+++ app/src/main/java/one/show/live/personal/view/VerifyCodeView.java
@@ -0,0 +1,116 @@
+package one.show.live.login.view;
+
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import one.show.live.R;
+
+
+public class VerifyCodeView extends RelativeLayout {
+    private EditText editText;
+    private TextView[] textViews;
+    private static int MAX = 6;
+    private String inputContent;
+    private InputMethodManager imm;
+
+    public VerifyCodeView(Context context) {
+        this(context, null);
+    }
+
+    public VerifyCodeView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public VerifyCodeView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        View.inflate(context, R.layout.view_verify_code, this);
+
+        textViews = new TextView[MAX];
+        textViews[0] = findViewById(R.id.tv_0);
+        textViews[1] = findViewById(R.id.tv_1);
+        textViews[2] = findViewById(R.id.tv_2);
+        textViews[3] = findViewById(R.id.tv_3);
+        textViews[4] = findViewById(R.id.tv_4);
+        textViews[5] = findViewById(R.id.tv_5);
+        editText = findViewById(R.id.edit_text_view);
+        setEditTextListener();
+    }
+
+    private void setEditTextListener() {
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                inputContent = editText.getText().toString();
+
+                if (inputCompleteListener != null) {
+                    if (inputContent.length() >= MAX) {
+                        inputCompleteListener.inputComplete();
+                    } else {
+                        inputCompleteListener.invalidContent();
+                    }
+                }
+
+                for (int i = 0; i < MAX; i++) {
+                    if (i < inputContent.length()) {
+                        textViews[i].setText(String.valueOf(inputContent.charAt(i)));
+                    } else {
+                        textViews[i].setText("");
+                    }
+                }
+            }
+        });
+    }
+
+
+    private InputCompleteListener inputCompleteListener;
+
+    public void setInputCompleteListener(InputCompleteListener inputCompleteListener) {
+        this.inputCompleteListener = inputCompleteListener;
+    }
+
+    public interface InputCompleteListener {
+
+        void inputComplete();
+
+        void invalidContent();
+    }
+
+    public String getEditContent() {
+        return inputContent;
+    }
+
+    /**
+     * 显示键盘
+     */
+    public void showSoftInput(Context context) {
+        //显示软键盘
+        imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null && editText != null) {
+            editText.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    imm.showSoftInput(editText, 0);
+                }
+            }, 200);
+        }
+    }
+
+}
