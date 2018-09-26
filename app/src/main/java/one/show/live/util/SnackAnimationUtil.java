b217d09cb6f631fd0619b9e37c845797dd69191a
lihui <hui_tianyi@163.com>
2018-09-26 14:39:29 +0800

    adds snacks

---
+++ app/src/main/java/one/show/live/util/SnackAnimationUtil.java
@@ -0,0 +1,98 @@
+package one.show.live.util;
+
+
+import android.content.Context;
+import android.graphics.Rect;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.ScrollView;
+
+public class SnackAnimationUtil  {
+    private View mTargetView;
+    private long mAutoDismissTime;
+    private Animation mSnackInAnim;
+    private Animation mSnackOutAnim;
+    private SnackAnimationCallback mSnackAnimationCallback;
+
+    public static SnackAnimationUtil with(Context context, @AnimRes int snackInRes, @AnimRes int snackOutRes) {
+        return new SnackAnimationUtil(context, snackInRes, snackOutRes);
+    }
+
+    private SnackAnimationUtil(Context context, @AnimRes int snackInRes, @AnimRes int snackOutRes) {
+        mSnackInAnim = AnimationUtils.loadAnimation(context, snackInRes);
+        mSnackOutAnim = AnimationUtils.loadAnimation(context, snackOutRes);
+    }
+
+    public SnackAnimationUtil setDismissDelayTime(long autoDismissTime) {
+        mAutoDismissTime = autoDismissTime;
+        return this;
+    }
+
+    public SnackAnimationUtil setTarget(View playView) {
+        mTargetView = playView;
+        return this;
+    }
+
+    public SnackAnimationUtil setDismissDelayCallback(SnackAnimationCallback snackAnimationCallback){
+        mSnackAnimationCallback = snackAnimationCallback;
+        return this;
+    }
+
+    public void play() {
+        if (mTargetView == null || mSnackInAnim == null || mSnackOutAnim == null) {
+            return;
+        }
+        mTargetView.setVisibility(View.VISIBLE);
+        mSnackInAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                playSnackOutAnimation();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mTargetView.startAnimation(mSnackInAnim);
+    }
+
+    private void playSnackOutAnimation() {
+        mSnackOutAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                mTargetView.setVisibility(View.GONE);
+                if (mSnackAnimationCallback != null) {
+                    mSnackAnimationCallback.dismissCallback();
+                }
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        mTargetView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mTargetView.startAnimation(mSnackOutAnim);
+            }
+        }, mAutoDismissTime);
+    }
+
+    public interface SnackAnimationCallback {
+        void dismissCallback();
+    }
+}
+
